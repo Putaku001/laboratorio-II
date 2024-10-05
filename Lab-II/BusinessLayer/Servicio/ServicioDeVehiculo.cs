2be3c645ee4fcd83bcbce75f4c0e95a35d711d73
Putaku001 <133188152+Putaku001@users.noreply.github.com>
2024-10-04 22:23:38 -0600

    Add files via upload

---
+++ Lab-II/BusinessLayer/Servicio/ServicioDeVehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DataAccessLayer.Repositorio;
+
+namespace BusinessLayer.Servicio
+{
+    public class ServicioDeVehiculo
+    {
+        private RepositorioDeVehiculos _RepositorioDeVehiculos;
+
+        public ServicioDeVehiculo()
+        {
+            _RepositorioDeVehiculos = new RepositorioDeVehiculos();
+        }
+        
+    }
+}
---
+++ Lab-II/DataAccessLayer/dbConnect/sqlConnect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System;
+
+
+namespace DataAccessLayer.dbConnect
+{
+    public class SqlConnect
+    {
+        private readonly string _connectionString;
+
+
+        public SqlConnect()
+        {
+            _connectionString = "Data Source=DESKTOP-OKJJS3Y\\SQLEXPRESS;Initial Catalog=RentaVehiculosDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
